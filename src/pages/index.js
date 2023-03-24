1da1e088ced0f778aa6edc37fc784b0c39725eaa
Ayush Tiwari <ayushtiwari26402@gmail.com>
2023-03-24 13:46:36 +0530

    add mui, icons, fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
