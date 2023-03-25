3e37ea67be2f06114aeb9e28d8c2d4bf99b45966
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-03-25 06:29:00 +0000

    Card Layout

---
+++ src/components/Mentor.js
@@ -0,0 +1,33 @@
+import { motion} from "framer-motion";
+var cardVariants = {
+    offscreen: {
+        y: 300
+    },
+    onscreen: {
+        y: 50,
+        rotate: -10,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8
+        }
+    }
+};
+var hue = function (h) { return "hsl(".concat(h, ", 0%, 0%)"); };
+function Mentor(Props){
+    const background = `linear-gradient(306deg, ${hue(Props.colorA)}, ${hue(Props.colorB)})`;
+    return (
+    <motion.div
+    className="card-container"
+    initial="offscreen"
+    whileInView="onscreen"
+    viewport={{ once: true, amount: 0.8 }}>
+    <div className="splash" style={{ background }}></div>
+    <motion.div className="card" variants={cardVariants}>
+      {Props.image}
+    </motion.div>
+  </motion.div>
+);
+}
+
+export default Mentor;
