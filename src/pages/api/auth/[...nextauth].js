1ae62155ca76d3ab31c97baff9a2656e4b621aaf
Ayush Tiwari <ayushtiwari26402@gmail.com>
2023-03-25 11:22:08 +0530

    add linkedin auth not active

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,17 +2,22 @@ import NextAuth from "next-auth"
 import process from "process"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import LinkedInProvider from "next-auth/providers/linkedin"
 
 export const authOptions = {
     // TODO: Configure one or more authentication providers
     providers: [
-        GithubProvider({
-            clientId: process.env.NEXTAUTH_OAUTH_GITHUB_ID,
-            clientSecret: process.env.NEXTAUTH_OAUTH_GITHUB_SECRET,
-        }),
         GoogleProvider({
-            clientId: process.env.NEXTAUTH_OAUTH_GOOGLE_CLIENT_ID,
-            clientSecret: process.env.NEXTAUTH_OAUTH_GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        // LinkedInProvider({
+        //     clientId: process.env.LINKEDIN_CLIENT_ID,
+        //     clientSecret: process.env.LINKEDIN_CLIENT_SECRET
+        // }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
         })
     ],
 }
