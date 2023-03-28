2ae47f5dc2f5af30b4c665c799501a6e96e2a2b6
Ayush Tiwari <ayushtiwari26402@gmail.com>
2023-03-28 17:05:21 +0530

    remove lint issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Head from 'next/head'
-import Image from 'next/image'
-import {Inter} from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Faq from '@/components/Faq';
-import Hero from '@/components/Hero';
-const inter = Inter({subsets: ['latin']})
+// import Navbar from '../components/composite/Navbar'
+import Faq from '@/components/composite/home/Faq';
+import Hero from '@/components/composite/home/Hero';
 
 export default function Home() {
     return (
@@ -23,10 +19,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
                 {/**Google tag (gtag.js)**/}
             </Head>
-            {/* <Navbar/> */}
-            {/* <Faq/> */}
+             {/*<Navbar/>*/}
+             <Faq/>
             <Hero/>
-            
         </>
     )
 }
