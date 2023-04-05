f7432a1dd3090de347512cfcad99d7e603bc283a
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-04-06 00:02:34 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,11 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Head from 'next/head'
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-import Image from 'next/image'
-import {Inter} from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-const inter = Inter({subsets: ['latin']})
-=======
->>>>>>> 071190f08143a650c34fa90fb8c78e9674cca92f
-=======
->>>>>>> Stashed changes
-// import Navbar from '../components/composite/Navbar'
-import Faq from '@/components/composite/home/Faq';
-import Hero from '@/components/composite/home/Hero';
+// import {Inter} from 'next/font/google'
+// import styles from '@/styles/Home.module.css'
+
+
+// const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
