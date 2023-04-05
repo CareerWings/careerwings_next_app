071190f08143a650c34fa90fb8c78e9674cca92f
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-04-05 17:03:50 +0000

    conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Head from 'next/head'
-// import {Inter} from 'next/font/google'
-// import styles from '@/styles/Home.module.css'
-
-
-// const inter = Inter({subsets: ['latin']})
+// import Navbar from '../components/composite/Navbar'
+import Faq from '@/components/composite/home/Faq';
+import Hero from '@/components/composite/home/Hero';
 
 export default function Home() {
     return (
@@ -21,12 +19,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
                 {/**Google tag (gtag.js)**/}
             </Head>
-
-
-           
-
-            
-    
+             {/*<Navbar/>*/}
+             <Faq/>
+            <Hero/>
         </>
     )
 }
