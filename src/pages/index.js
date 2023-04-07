2b3abcb1c4e3f6b8c3366ea0d4e59ed1168875af
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-04-07 12:38:39 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-=======
 import Head from 'next/head'
 // import Navbar from '../components/composite/Navbar'
 import Faq from '@/components/composite/home/Faq';
